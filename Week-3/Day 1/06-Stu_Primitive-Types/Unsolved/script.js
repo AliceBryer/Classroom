b54354070deecb1a95ae5c646dd3178bdc15a31f
Alice Bryer <alicebryer123@gmail.com>
2022-04-12 20:25:49 +0100

    folder structure changed

---
+++ Week-3/Day 1/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,23 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console//
+console.log(typeof one); //This will show number//
+console.log(typeof two); //This will show string//
+console.log(typeof three); //This will show boolean//
+console.log(typeof four); //This will show number//
+console.log(typeof five);//This will show string //
+console.log(typeof six);//This will show Undefined//
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four); //This will show string//
+console.log(typeof five); // This will show boolean//
+console.log(typeof six);//This will show number//
