6d8f35011f9e4db1e586bff4198f601325d84272
Alice Bryer <alicebryer123@gmail.com>
2022-04-14 20:06:00 +0100

    break commit

---
+++ Week-3/Day 2/12-Stu_Arrays/Unsolved/script.js
@@ -0,0 +1,31 @@
+// // WRITE YOUR CODE HERE
+
+// //1
+// let students = ["Matthew", "Bob", "Sally", "Emma", "Mohammed"]
+// console.log (students.length)
+
+// //2
+// console.log("Welcome to the class", students[0]);
+// console.log("Welcome to the class", students[1]);
+// console.log("Welcome to the class", students[2]);
+// console.log("Welcome to the class", students[3]);
+// console.log("Welcome to the class", students[4]);
+
+// //3 - It's done when the first element in the array is replaced with the name of a new student.
+
+// students[0] ="New Student";
+// console.log(students);
+
+// //4
+// //It's done when, after an if statement confirms that the first element in the array 
+// //has been replaced, the message "REPLACED_NAME is in class" is logged.
+
+// if (students[0] === "New Student") {
+//     console.log (students[0], "is in class");
+// }
+
+// printing 10 down
+for (let i=10; i>0; i--){
+    console.log(i);
+}
+
---
+++ Week-3/Day 2/14-Stu_Iteration/Unsolved/script.js
@@ -0,0 +1,22 @@
+//WRITE YOUR CODE BELOW
+
+var students = ["Bob", "Bobby", "Robert", "Dave", "David"]
+
+console.log (students);
+
+console.log (students.length)
+
+// * It's done when, using a `for` loop, the greeting
+//  "Great to see you, CLASSMATE_NAME!" logs to the console
+//   for each classmate's name in the `students` array. 
+
+// for (i=[0]; i<5; i++) {
+// console.log ("Great to see you",students[i],"!")
+// }
+
+// while (i < 10) {
+//     text += "The number is " + i;
+//     i++;
+//   }
+
+
